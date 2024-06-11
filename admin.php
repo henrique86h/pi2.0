0e646df7d67be1e1e1b98b61172586f0618d97ce
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-10 21:22:09 -0300

    comecinho

---
+++ admin.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="henrique.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tiny5&display=swap" rel="stylesheet">
+    <title>Admin - Juegos</title>
+</head>
+<body>
+<main>
+    <section class="container-admin">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Banner</th>
+                    <th>Plataformas</th>
+                    <th colspan= "3">Ações</th>
+                </tr>
+            </thead>
+        </table>
+        <tbody>
+            <?php
+            include "conexao.php";
+            $sql = "select * from tb_pi";
+            $resultado = mysqli_query($conexao, $sql);
+            
+            while ($jogos = mysqli_fetch_assoc($resultado)):
+            ?>
+
+                <tr>
+                    <td><?php echo $jogos['nome'] ?></td>
+                    <td><?php echo $jogos['banner'] ?></td>
+                    <td><?php echo $jogos['plataformas'] ?></td>
+                    <td><a class="botao-editar" href="editar.php?id=<?php echo $produtos['id'] ?>">Editar</a></td>
+                    <td>
+                        <a href="excluir.php?id=<?php echo $produtos['id'] ?>">
+                        <input type="button" class="botao-excluir" value="excluir">
+                        </a>
+                        
+                    </td>
+                </tr>
+
+
+        </tbody>
+    </section>
+
+
+</main>   
+</body>
+</html>
---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$bd = "bd_pi";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $bd, 3306);
+
+?>
