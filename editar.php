e57437d11fbfc36b1baacbb4c50287b7ab728fe3
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-21 22:14:45 -0300

    começo editar

--- editar.php
+++ editar.php
@@ -27,8 +27,53 @@
             $nintendo = $jogo['nintendo'];
             $pc = $jogo['pc'];
         }
-
         ?>
+
+        <form method="post" enctype="multipart/form-data" action="editar-salvar.php?id=<?php echo $id ?>">
+
+        <label for="nome">Nome</label>
+
+                <input type="text" id="nome" name="nome" placeholder="Nome de um jogo" value="<?php echo $jogo ?>" required>
+                <div class="container-plataformas">
+                    <div>
+                        <label for="xbox">
+                        <input type="checkbox" id="xbox" name="xbox" value="<?php echo $jogo ?>">
+                    <img src="img/xbox.png" width=40></label>
+                    </div>
+                    <div>
+                        <label for="playstation">        
+
+                                <?php if($playstation == 'playstation'){ ?>
+                                    <input type="checkbox" id="playstation" name="playstation" value="playstation" checked>
+                                <?php }else{ ?>
+                                    <input type="checkbox" id="playstation" name="playstation" value="playstation">
+                                <?php } ?>
+
+                    <img src="img/playstation.png" width=40></label>
+                    </div>
+                    <div>
+                        <label for="nintendo">
+                        <input type="checkbox" id="nintendo" name="nintendo" value="nintendo">
+                    <img src="img/nintendo.png" width=40></label>
+                    </div>
+                    <div>
+                        <label for="pc">
+                        <input type="checkbox" id="pc" name="pc" value="pc">
+                    <img src="img/pc.png" width=40></label>
+                    </div>
+                </div>
+                <label for="descricao">Descrição</label>
+                <input type="text" id="descricao" name="descricao" placeholder="Digite uma descrição">
+
+                <br>
+
+                <label for="banner">Banner do jogo</label>
+                <input type="file" name="banner" names="banner" accept="image/*" id="banner" placeholder="Banner">
+
+                <br>
+
+                <input type="submit" name="editar" class="botao-adicionar" value="Editar jogo">
+        </form>
 
         </section>
     </main>
