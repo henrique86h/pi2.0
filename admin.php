1609948ebc071010b2f157ef73d818e373bcac4d
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-06-30 21:29:03 -0300

    ..

--- admin.php
+++ admin.php
@@ -1,4 +1,14 @@
-<?php include "cabecalho.php" ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="henrique.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
     <title>Admin - Juegos</title>
 <main>
 
@@ -85,5 +95,5 @@
         <script src="carrossel.js"></script>
     </section>
 </main>   
-
-<?php include "rodape.php" ?>
+</body>
+</html>
