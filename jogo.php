ffaee7518de88ddd7f67cb9020756aafc2e48ecb
Shadowind201 <enzocarnier@gmail.com>
2024-07-01 22:52:38 -0300

    detalhes

--- jogo.php
+++ jogo.php
@@ -27,11 +27,13 @@
 
 <h3>Trailer</h3>
 
-<iframe width="560" height="315" src="<?php echo $jogos['trailer'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<br>
+
+<iframe class="trailer" width="560" height="315" src="<?php echo $jogos['trailer'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 <br><br>
 
-<h3>Plataformas disponiveis:</h3>
+<h3>Plataformas disponíveis:</h3>
 
 <br>
 
